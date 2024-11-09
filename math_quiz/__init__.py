eb45a8a06a746ae500f88b9dfb0596e3e3ace284
Neelesh Babu <nneelesh31.12@gmail.com>
2024-11-09 13:50:18 +0100

    Update __init__.py

--- math_quiz/__init__.py
+++ math_quiz/__init__.py
@@ -1,1 +1,5 @@
+# math_quiz/__init__.py
+
+# Any code in this file will run when the package is imported
+# Usually, this file can be empty, but you could initialize certain things here if needed.
 
